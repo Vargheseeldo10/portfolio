c50698e61dd6b2a8d82517ea94e0e177ed143aec
Varghese Eldo <130234052+Vargheseeldo10@users.noreply.github.com>
2025-03-05 10:22:20 +0530

    Update ProjectCard.jsx

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -7,44 +7,36 @@ const ProjectCard = ({
   demoLink, 
   sourceCodeLink 
 }) => {
-  const handleExternalDemo = () => {
-    if (demoLink) {
-      window.location.href = demoLink;
-    }
-  };
-
-  const handleExternalSourceCode = () => {
-    if (sourceCodeLink) {
-      window.location.href = sourceCodeLink;
-    }
-  };
-
   return (
-    <div className="p-3 md:p-6 flex flex-col w-full md:w-1/2 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl h-full">
+    <div className="p-3 md:p-6 flex flex-col w-full md:w-full bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl h-full max-w-[700px]">
       <img 
-        className="p-4 w-full h-48 object-cover rounded-lg" 
+        className="p-4 w-full h-64 object-cover rounded-lg" 
         src={bannerImage} 
         alt={title} 
       />
       <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal">
         {title}
       </h3>
-      <p className="px-4 text-sm md:text-md leading-tight py-2 flex-grow"> {/* Flex-grow for consistent height */}
+      <p className="px-4 text-sm md:text-md leading-tight py-2 flex-grow">
         {main}
       </p>
-      <div className="mt-auto p-2 md:p-4 flex gap-2 md:gap-4"> {/* Buttons at the bottom */}
-        <button 
-          onClick={handleExternalDemo}
+      <div className="mt-auto p-2 md:p-4 flex gap-2 md:gap-4">
+        <a 
+          href={demoLink}
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]"
         >
           Demo
-        </button>
-        <button 
-          onClick={handleExternalSourceCode}
+        </a>
+        <a 
+          href={sourceCodeLink}
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]"
         >
           Source Code
-        </button>
+        </a>
       </div>
     </div>
   );
